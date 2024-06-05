8c21762c1e90df33f6df2070c93aa57b18d838d2
MINJUN YU <minjoon0524@gmail.com>
2024-06-05 22:52:31 +0900

    refactor:코드 최신화

--- src/main/java/plan/trip/config/SecurityConfig.java
+++ src/main/java/plan/trip/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package plan.trip.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,25 +22,30 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig  {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .formLogin(form -> form
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                // 폼로그인
+                .formLogin((form)-> form
+                        //.loginPage("/member/login")
                         .defaultSuccessUrl("/", true)
-                        .permitAll())
-                .logout(logout -> logout
+                )
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/", "/member/**").permitAll()
+                        .anyRequest().authenticated())
+                // 로그아웃
+                .logout((logout) -> logout
                         .logoutUrl("/member/logout")
                         .logoutSuccessUrl("/")
                         .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID")
-                        .permitAll())
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/", "/member/**").permitAll()
-                        .anyRequest().authenticated());
+                )
+                .sessionManagement(management ->management
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
@@ -66,5 +77,8 @@ public class SecurityConfig {
         filter.setForceEncoding(true);
         return filter;
     }
+
+
+
 
 }
